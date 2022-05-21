802e50cf0fefe2ad93fe85b622b5edb4adc3ae2d
Chugil.C <chugilrc13@gmail.com>
2022-05-21 12:32:05 +0530

    Completed Day 1

--- Day-001/main.py
+++ Day-001/main.py
@@ -1,1 +1,6 @@
-print("hello")
+# Band name generator project - Day 1
+print("Welcome to the Band Name Generator")
+name = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet name?\n")
+band_name = name +" "+ pet
+print("Your Band name will be "+ band_name)
