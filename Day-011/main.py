73c4399472954bfcac9979ccaf9372dbc8594b97
Chugil.C <chugilrc13@gmail.com>
2022-05-31 14:38:26 +0530

    Day 11 Completed

---
+++ Day-011/main.py
@@ -0,0 +1,85 @@
+# Day 11 - BlackJack Game
+from asyncio import current_task
+from os import system
+from art import logo
+import random
+############### Our Blackjack House Rules #####################
+
+## The deck is unlimited in size. 
+## There are no jokers. 
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+## The computer is the dealer.
+
+def deal_card():
+    cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+    card = random.choice(cards)
+    return card
+
+def calculate_score(cards):
+    if 11 in cards and 10 in cards and len(cards) == 2:
+        return 0
+    
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    
+    return sum(cards)
+
+def compare(user_score, computer_score):
+    if user_score == computer_score:
+        return "Draw"
+    elif computer_score == 0:
+        return "You Lost, Computer has a BlackJack!"
+    elif user_score == 0:
+        return "You Win a BlackJack"
+    elif user_score > 21:
+        return "You went over, You Lose"
+    elif computer_score > 21:
+        return "Opponent went over, You Win"
+    elif user_score > computer_score:
+        return "You Win"
+    else:
+        return "You Lose"
+def play_game():
+    user_cards = []
+    computer_cards = []
+    is_game_over = False
+
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
+
+    while not is_game_over:
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+
+        print(f"Your cards: {user_cards}, Your score: {user_score}")
+        print(f"Computer's first card: {computer_cards[0]}")
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
+        else:
+            user_should_deal = input("Type 'y' to get another card, type 'n' to pass ")
+
+            if user_should_deal == 'y':
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
+
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_card())
+        computer_score = calculate_score(computer_cards)
+
+    print(f"Your final hand {user_cards}, Final score: {user_score}")
+    print(f"Computer's final score {computer_cards}, Final score: {computer_score}")
+    print(compare(user_score, computer_score))
+
+while input("Do you want to play a game of BlackJack? Type 'y' or 'n' ") == 'y':
+    system('cls')
+    print(logo)
+    play_game()
