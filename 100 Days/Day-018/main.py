47fc52cfb9151b668e19fb14cecca9b92fa87abb
Chugil.C <chugilrc13@gmail.com>
2022-06-07 15:14:37 +0530

    Day 18 Completed

---
+++ 100 Days/Day-018/main.py
@@ -0,0 +1,37 @@
+from turtle import Turtle, Screen
+import random
+import turtle
+
+# Create the turtle
+timmy = Turtle()
+timmy.speed('fastest')
+timmy.penup()
+timmy.hideturtle()
+
+turtle.colormode(255)
+
+color_list = [(246, 245, 243), (233, 239, 246), (246, 239, 242), (240, 246, 243), (132, 166, 205), (221, 148, 106), (32, 42, 61), (199, 135, 148), (166, 58, 48), (141, 184, 162), (39, 105, 157), (237, 212, 90), (150, 59, 66), (216, 82, 71), (168, 29, 33), (235, 165, 157), (51, 111, 90), (35, 61, 55), (156, 33, 31), (17, 97, 71), (52, 44, 49), (230, 161, 166), (170, 188, 221), (57, 51, 48), (184, 103, 113), (32, 60, 109), (105, 126, 159), (175, 200, 188), (34, 151, 210), 
+(65, 66, 56)]
+
+timmy.setheading(225)
+timmy.forward(300)
+timmy.setheading(0)
+
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    timmy.dot(20, random.choice(color_list))
+    timmy.forward(50)
+
+    if dot_count % 10 == 0:
+        timmy.setheading(90)
+        timmy.forward(50)
+        timmy.setheading(180)
+        timmy.forward(500)
+        timmy.setheading(0)
+
+
+
+# Create the screen
+screen = Screen()
+screen.exitonclick()
