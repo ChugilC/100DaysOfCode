8750b1cc8e0cb1146e5c464f8e0376f0571f0810
Chugil.C <chugilrc13@gmail.com>
2022-06-13 08:07:13 +0530

    Added highscore feature to the game

--- 100 Days/Day-021/scoreboard.py
+++ 100 Days/Day-021/scoreboard.py
@@ -5,6 +5,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("100 Days\Day-021\data.txt") as data:
+            self.highscore = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -12,13 +14,18 @@ class ScoreBoard(Turtle):
         self.update_score()
 
     def update_score(self):
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 20, "normal"))
+        self.clear()
+        self.write(f"Score: {self.score} HighScore: {self.highscore}", align="center", font=("Arial", 20, "normal"))
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align="center", font=("Arial", 20, "normal"))
+    def reset(self):
+        
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("100 Days\Day-021\data.txt", mode="w") as data:
+                data.write(f"{self.highscore}")
+        self.score = 0
+        self.update_score()
     
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_score()
