77ffe5ca16258d92038b2fc2ac9222d19b1342ab
Chugil.C <chugilrc13@gmail.com>
2022-06-18 12:14:54 +0530

    Day 29 Completed
    Password Manager

---
+++ 100 Days/Day-029/main.py
@@ -0,0 +1,84 @@
+from secrets import choice
+from tkinter import *
+from tkinter import messagebox
+from random import randint, choice, shuffle
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    password_entry.clipboard_append(password)
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if website == "" or password == "" or email == "":
+        messagebox.showinfo(title="No data", message="Please fill all the fields")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}\nPassword: {password} \n Is it ok to save?")
+
+        if is_ok:
+            with open("100 Days/Day-029/data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+                website_entry.focus()
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+# Canvas
+canvas = Canvas(width=200, height=200)
+lock_image = PhotoImage(file="100 Days/Day-029/logo.png")
+canvas.create_image(100, 100, image=lock_image)
+canvas.grid(column=1, row=0)
+
+# Labels
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+# Entries
+website_entry = Entry()
+website_entry.grid(column=1, row=1,columnspan=2, sticky="EW")
+website_entry.focus()
+
+email_entry = Entry()
+email_entry.grid(column=1, row=2,columnspan=2, sticky="EW")
+email_entry.insert(0, "jos@boss.com")
+
+password_entry = Entry()
+password_entry.grid(column=1, row=3, sticky="EW")
+
+# Buttons
+generate_password_button = Button(text="Generate", command=generate_password)
+generate_password_button.grid(column=2, row=3, sticky="EW")
+
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(column=1, row=4, columnspan=2, sticky="EW")
+
+
+window.mainloop()
