f997ce36a37dadef4b351c9a80e66b3b4e3803f8
Chugil.C <chugilrc13@gmail.com>
2022-06-19 11:25:19 +0530

    Refactored: Added exception handling

--- 100 Days/Day-026/main.py
+++ 100 Days/Day-026/main.py
@@ -7,6 +7,20 @@ data = pandas.read_csv("100 Days/Day-026/nato_phonetic_alphabet.csv")
 data_dict = {row["letter"]: row["code"] for (index, row) in data.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-nato_alpabets = [data_dict[char] for char in word]
+is_on = True
+
+while is_on:
+    word = input("Enter a word: ").upper()
+
+    nato_alpabets = []
+    for char in word:
+        try:
+            seperate_word = data_dict[char]
+        except KeyError:
+            print("Please provide only alphabets")
+            break
+        else:
+            nato_alpabets.append(seperate_word)
+            is_on = False
+
 print(nato_alpabets)
