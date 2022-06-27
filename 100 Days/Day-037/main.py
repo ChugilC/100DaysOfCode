3dbfe88815383fa7598d2df646a4d87f9371ffd4
Chugil.C <chugilrc13@gmail.com>
2022-06-27 22:32:48 +0530

    Day 37 Started

---
+++ 100 Days/Day-037/main.py
@@ -0,0 +1,33 @@
+from traceback import print_stack
+import requests
+
+pixela_endpoint = "https://pixe.la/v1/users"
+USERNAME = "chugil"
+TOKEN = "100DaysOfPython"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Running Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "sora"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+print(response.text)
