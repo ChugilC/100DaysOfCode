3b7c9a1ca75f5ed5f2e80e65160135fb8ddef6ed
Chugil.C <chugilrc13@gmail.com>
2022-06-28 09:34:39 +0530

    Day 37 Completed

--- 100 Days/Day-037/main.py
+++ 100 Days/Day-037/main.py
@@ -1,9 +1,11 @@
-from traceback import print_stack
 import requests
+from datetime import datetime
 
 pixela_endpoint = "https://pixe.la/v1/users"
 USERNAME = "chugil"
 TOKEN = "100DaysOfPython"
+GRAPHID = "graph1"
+NAME = "Running Graph"
 
 user_params = {
     "token": TOKEN,
@@ -12,14 +14,15 @@ user_params = {
     "notMinor": "yes"
 }
 
+# CREATE user account 
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
 
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": "graph1",
-    "name": "Running Graph",
+    "id": GRAPHID,
+    "name": NAME,
     "unit": "Km",
     "type": "float",
     "color": "sora"
@@ -29,5 +32,36 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-print(response.text)
+# CREATE graph
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+today = datetime.now()
+
+# POST value to the graph
+graph_value_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPHID}"
+
+graph_value_config = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "3.0"
+
+}
+
+# response = requests.post(url=graph_value_endpoint,json=graph_value_config ,headers=headers)
+# print(response.text)
+
+# UPDATE graph
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPHID}/{today.strftime('%Y%m%d')}"
+
+update_config = {
+    "quantity": "8.0"
+}
+
+# response = requests.put(url=update_endpoint, json=update_config, headers=headers)
+# print(response.text)
+
+# DELETE pixela
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPHID}/{today.strftime('%Y%m%d')}"
+
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
