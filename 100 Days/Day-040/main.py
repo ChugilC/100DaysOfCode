d8c8f51f15e5b3a2954f037ce7a186f3854bcdd9
Chugil.C <chugilrc13@gmail.com>
2022-07-01 21:34:37 +0530

    Project Planning Day 40
    Using Spotify API

---
+++ 100 Days/Day-040/main.py
@@ -0,0 +1,7 @@
+# Day 40 - Project
+
+# Use Spotify API to create a playlist using python
+# User enter a playlist name - Create it
+# Take the user input for song name and add it to the playlist
+
+# Docs: https://developer.spotify.com/documentation/web-api/
