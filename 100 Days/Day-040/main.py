706b4726fbbbbff5453ff3b3b97b24212df252e5
Chugil.C <chugilrc13@gmail.com>
2022-07-02 22:25:32 +0530

    Started with pygame module

--- 100 Days/Day-040/main.py
+++ 100 Days/Day-040/main.py
@@ -1,7 +1,40 @@
-# Day 40 - Project
+import pygame
+import os
 
-# Use Spotify API to create a playlist using python
-# User enter a playlist name - Create it
-# Take the user input for song name and add it to the playlist
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT)) # Actual window
+pygame.display.set_caption("Space Game")
 
-# Docs: https://developer.spotify.com/documentation/web-api/
+
+WHITE = (255, 255, 255)
+
+FPS = 60
+
+YELLOW_SPACESHIP = pygame.image.load(os.path.join("Assets", "spaceship_yellow.png"))
+RED_SPACESHIP = pygame.image.load(os.path.join("Assets", "spaceship_red.png"))
+
+def draw_window():
+        WIN.fill(WHITE) # Change the color
+        WIN.blit(YELLOW_SPACESHIP)
+
+        pygame.display.update() # Update the screen 
+
+
+def main():
+    clock = pygame.time.Clock()
+    run = True
+
+    while run:
+        clock.tick(FPS)
+        
+        for event in pygame.event.get():
+            # Handling quit event
+            if event.type == pygame.QUIT:
+                run = False
+        
+        draw_window()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
